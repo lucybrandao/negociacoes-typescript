f9b596f30aedc4c91e00d14abc9bd8f161d4ba63
Lucy Brandão <lucyjbrandao@gmail.com>
2022-09-05 13:18:59 -0300

    lapidando o código

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -14,4 +14,13 @@ export class Negociacao {
         const data = new Date(this._data.getTime());
         return this._data;
     }
+
+    public static criaDe(dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+
+        return new Negociacao(date, quantidade, valor);
+    }
 }
